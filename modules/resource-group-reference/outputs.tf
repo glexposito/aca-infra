150157284ca58e2d300235dc603902f4a2271c72
Guillermo Luis Exposito <glexposito@gmail.com>
2026-03-22 12:34:36 +1300

    chore: remove unused resource-group-reference module

--- modules/resource-group-reference/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "resource_group_name" {
-  description = "Existing resource group name."
-  value       = var.resource_group_name
-}
-
-output "resource_group_location" {
-  description = "Existing resource group location."
-  value       = var.resource_group_location
-}
--- modules/resource-group-reference/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "resource_group_name" {
-  description = "Existing resource group name."
-  type        = string
-}
-
-variable "resource_group_location" {
-  description = "Existing resource group location."
-  type        = string
-}
